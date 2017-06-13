d0f9d714457f96384ee628f291b9bfd8e58561ef
saqibkh <saqibkhan@utexas.edu>
2017-06-13 09:25:10 -0500

    Create activation.hpp

--- activation.hpp
+++ activation.hpp
@@ -3,6 +3,7 @@
 #include <sdbusplus/server.hpp>
 #include <xyz/openbmc_project/Software/Activation/server.hpp>
 #include <xyz/openbmc_project/Software/ActivationBlocksTransition/server.hpp>
+#include "xyz/openbmc_project/Software/RedundancyPriority/server.hpp"
 
 namespace phosphor
 {
@@ -15,7 +16,9 @@ using ActivationInherit = sdbusplus::server::object::object<
     sdbusplus::xyz::openbmc_project::Software::server::Activation>;
 using ActivationBlocksTransitionInherit = sdbusplus::server::object::object<
  sdbusplus::xyz::openbmc_project::Software::server::ActivationBlocksTransition>;
-
+using RedundancyPriorityInherit = sdbusplus::server::object::object<
+    sdbusplus::xyz::openbmc_project::Software::server::RedundancyPriority>;
+    
 /** @class ActivationBlocksTransition
  *  @brief OpenBMC ActivationBlocksTransition implementation.
  *  @details A concrete implementation for
@@ -34,6 +37,45 @@ class ActivationBlocksTransition : public ActivationBlocksTransitionInherit
                    ActivationBlocksTransitionInherit(bus, path.c_str()) {}
 };
 
+/** @class Activation
+ *  @brief OpenBMC activation software management implementation.
+ *  @details A concrete implementation for
+ *  xyz.openbmc_project.Software.Activation DBus API.
+ */
+class RedundancyPriority : public RedundancyPriorityInherit
+{
+    public:
+        /** @brief Constructs RedundancyPriority.
+         *
+         *  @param[in] bus    - The Dbus bus object
+         *  @param[in] path   - The Dbus object path
+         */
+        RedundancyPriority(sdbusplus::bus::bus& bus,
+                                   const std::string& path) :
+                                   RedundancyPriorityInherit(bus,
+                                   path.c_str(), true),
+                                   bus(bus)
+        {
+            // Set Property
+            priority(0);
+            // Emit deferred signal.
+            emit_object_added();
+        }
+
+        /** @brief Overloaded priority property setter function
+         *
+         *  @param[in] value - One of RedundancyPriority::Priority
+         *
+         *  @return Success or exception thrown
+         */
+        uint8_t priority(uint8_t value) override;
+
+        /** @brief Persistent sdbusplus DBus bus connection */
+        sdbusplus::bus::bus& bus;
+
+        void freePriority(uint8_t basePriority);
+};
+    
 /** @class Activation
  *  @brief OpenBMC activation software management implementation.
  *  @details A concrete implementation for
@@ -92,6 +134,10 @@ class Activation : public ActivationInherit
 
         /** @brief Persistent ActivationBlocksTransition dbus object */
         std::unique_ptr<ActivationBlocksTransition> activationBlocksTransition;
+        
+        /** @brief Persistent RedundancyPriority dbus object */
+        std::unique_ptr<RedundancyPriority> redundancyPriority;
+
 };
 
 } // namespace updater
